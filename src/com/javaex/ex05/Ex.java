9b4c44dde9f3cc08bb2ccdaa6be74164eb11b1de
soyoungJin44 <wlsthdud0412@naver.com>
2024-06-28 10:02:50 +0900

    math.random() 으로 수정

--- src/com/javaex/ex05/Ex.java
+++ src/com/javaex/ex05/Ex.java
@@ -4,7 +4,11 @@ public class Ex {
 
 	public static void main(String[] args) {
 
-		
+		for(int i = 1; i <= 6; i++) {
+			System.out.print((int)(Math.random() * 45)+1);
+			System.out.print("	");
+			
+		}
 		
 	}
 
